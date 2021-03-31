0f228fd12602bc7451c3a466cc838d683192c164
abubaca4 <abubaca4@gmail.com>
2021-03-31 13:45:17 +0300

    test

---
+++ 2021_inform_protect/lab1.js
@@ -0,0 +1,20 @@
+var a_data = document.getElementById('id_a_data');
+var c_data = document.getElementById('id_c_data');
+var interval_data = document.getElementById('id_interval_data');
+var count_data = document.getElementById('id_count_data');
+
+function get_xn(a, xn, c, m) {
+    return (a * xn + c) % m;
+}
+
+function get_lcg() {
+    var result = [];
+    var xn = 0;
+    for (var i = 0; i < parseInt(count_data.value); i++) {
+        xn = get_xn(parseInt(a_data.value), xn, parseInt(c_data.value), parseInt(interval_data.value));
+        result.push(xn);
+    }
+    return result;
+}
+
+document.getElementById("lcg_text_result").innerHTML = get_lcg();
