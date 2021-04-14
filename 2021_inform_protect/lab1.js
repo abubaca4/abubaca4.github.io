087f9611a4d819830cf895b9828bbb7f6ba23731
abubaca4 <abubaca4@gmail.com>
2021-04-14 10:21:53 +0300

    lab 1 part 2

--- 2021_inform_protect/lab1.js
+++ 2021_inform_protect/lab1.js
@@ -2,40 +2,96 @@ var a_data = document.getElementById('id_a_data');
 var c_data = document.getElementById('id_c_data');
 var interval_data = document.getElementById('id_interval_data');
 var count_data = document.getElementById('id_count_data');
+var p_data = document.getElementById('id_p_data');
+var q_data = document.getElementById('id_q_data');
 
-function get_xn(a, xn, c, m) {
+function lcg_get_xn(a, xn, c, m) {
     return (a * xn + c) % m;
 }
 
 function get_lcg() {
-    var result = [];
-    var xn = 0;
-    for (var i = 0; i < parseInt(count_data.value); i++) {
-        xn = get_xn(parseInt(a_data.value), xn, parseInt(c_data.value), parseInt(interval_data.value));
+    let result = [];
+    let xn = 0;
+    let a = parseInt(a_data.value);
+    let c = parseInt(c_data.value);
+    let interval = parseInt(interval_data.value);
+    let count = parseInt(count_data.value);
+    for (let i = 0; i < count; i++) {
+        xn = lcg_get_xn(a, xn, c, interval);
         result.push(xn);
     }
     return result;
 }
 
-function update_lcd() {
-    var data = get_lcg();
-    document.getElementById("lcg_text_result").innerHTML = data.join("<br/>");
-    var trace = {
+function show_gist(taget_div, data) {
+    let trace = {
         x: data,
         type: 'histogram',
     };
-    var parent_width = document.getElementById("lcg_gist_result").offsetWidth;
+    let parent_width = document.getElementById(taget_div).offsetWidth;
     let layout = {
         width: parent_width - 15,
         responsive: true,
-        margin: { t: 0, b: 30, l: 10, r: 0 },
+        margin: { t: 0, b: 30, l: 15, r: 0 },
     }
-    Plotly.newPlot('lcg_gist_result', [trace], layout);
+    Plotly.newPlot(taget_div, [trace], layout);
+}
+
+function update_lcd() {
+    let data = get_lcg();
+    document.getElementById("lcg_text_result").innerHTML = data.join("<br/>");
+    show_gist("lcg_gist_result", data);
 }
 
 update_lcd();
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function isCoprime(a, b) {
+    for (let gcd = a; ; gcd = b, b = a % b, a = gcd)
+        if (!b) return gcd == 1;
+}
+
+function bbs_get_xn(x, M) {
+    return (x * x) % M;
+}
+
+function get_bbs() {
+    let result = [];
+    let p = parseInt(p_data.value);
+    let q = parseInt(q_data.value);
+    let interval = parseInt(interval_data.value);
+    let count = parseInt(count_data.value);
+    let M = p * q;
+    let x;
+    do {
+        x = getRandomInt(M);
+    } while (isCoprime(x, M));
+    x = bbs_get_xn(x, M);
+    for (let i = 0; i < count; i++) {
+        x = bbs_get_xn(x, M);
+        result.push(x % interval);
+    }
+    return result;
+}
+
+function update_bbs() {
+    let data = get_bbs();
+    document.getElementById("bbs_text_result").innerHTML = data.join("<br/>");
+    show_gist("bbs_gist_result", data);
+}
+
+update_bbs();
+
 a_data.addEventListener("change", update_lcd);
 c_data.addEventListener("change", update_lcd);
-interval_data.addEventListener("change", update_lcd);
-count_data.addEventListener("change", update_lcd);
+interval_data.addEventListener("change", function () {
+    update_lcd();
+    update_bbs();
+});
+count_data.addEventListener("change", function () {
+    update_lcd();
+    update_bbs();
+});
